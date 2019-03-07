6bab3efbc6df052b64be6ec43649ba9c27233f06
willbritt <47224666+willbritt@users.noreply.github.com>
2019-03-07 01:16:53 -0500

    Update makeChart.js

--- makeChart.js
+++ makeChart.js
@@ -14,7 +14,6 @@ var drawChart = function(data)
 {
   var width = 1000;
   var height = 500;
-  va
   var barWidth = width/data.length;
   var svg = d3.select("svg")
               .attr("height", height)
@@ -34,19 +33,7 @@ var drawChart = function(data)
      .attr("fill", function(d)
       { return d.Color;})
   
-  svg.selectAll("text")
-   .data(data)
-   .enter()
-   .append("text")
-   .text(function(d)
-      { return d.W;})
-   .attr("x", function(d,i)
-      { return (i * barWidth) + 20;})
-   .attr("y", function(d)
-      { return height - (d.W*10)+15;})
-   .attr("fill", function(d) 
-      {return "white";})
-   .attr("font-weight", "bold")
+  
   
   svg.selectAll("text")
    .data(data)
